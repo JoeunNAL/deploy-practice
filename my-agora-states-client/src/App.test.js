8ad210e79bbd75b9a6bf2cef1379c41d30275d67
Eunjin Jo <euji42@gmail.com>
2022-08-08 23:18:55 +0900

    fix: vercel 테스트용 test 통과 후 빌드 적용

--- my-agora-states-client/src/App.test.js
+++ my-agora-states-client/src/App.test.js
@@ -1,5 +1,10 @@
+import { render } from "@testing-library/react";
+// import
+import App from "./App";
+
 describe("Vercel 테스트용", () => {
-  test("실패", () => {
-    return false;
+  test("My Agora States 텍스트 노드가 있다.", () => {
+    const { queryByText } = render(<App />);
+    expect(queryByText(/^My Agora States$/)).toBeTruthy();
   });
 });
